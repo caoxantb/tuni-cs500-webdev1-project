646fabae579a406e2d63bd65566e2254897dcd88
Xuan-An Cao <an.cao@tuni.fi>
2023-11-09 11:21:48 +0200

    refactor: user schema

--- models/user.js
+++ models/user.js
@@ -69,7 +69,7 @@ const userSchema = new Schema({
     required: true,
     minLength: SCHEMA_DEFAULTS.password.minLength,
     set: password => {
-      if (!password || password === undefined || password.length < 10) {
+      if (!password || password.length < 10) {
         return password;
       }
       return bcrypt.hashSync(password, SALT_ROUNDS);
@@ -104,7 +104,7 @@ userSchema.methods.checkPassword = async function(password) {
   //      - password as given as parameter to the call to this method
   //      - the password of the user from the User model (this.password). 
   //          Here we see one of the few places where we need to use 'this' keyword.
-  return bcrypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 
 // Omit the version key when serialized to JSON
