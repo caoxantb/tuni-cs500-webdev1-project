2ed002b2f6c34a5da1217238d9d1fc536935f7ce
Xuan-An Cao <an.cao@tuni.fi>
2023-11-17 04:07:33 +0200

    fix: use chai instead of assert for test/own & remove paths for testing

--- test/own/test.js
+++ test/own/test.js
@@ -1,85 +1,80 @@
-const assert = require('assert'); // You can remove this line if not needed
 const { describe, it } = require('mocha');
-
-// Import the modules/classes/functions you are testing
-const routes = require('../routes'); // Adjust the path as needed
-const usersController = require('../controllers/users'); // Adjust the path as needed
-const productsController = require('../controllers/products'); // Adjust the path as needed
+const { expect } = require('chai'); // Use chai's expect interface
 
 describe('Routes', () => {
     it('Issue1: Remove unnecessary requires and unused variables from routes.js', () => {
         // Placeholder test, replace with actual test code
-        assert.throws(() => {
+        expect(() => {
             throw new Error('Not implemented');
-        });
+        }).to.throw();
     });
 });
 
 describe('Test Directory', () => {
     it('Issue2: Create a new subdirectory /own under the /test directory', () => {
         // Placeholder test, replace with actual test code
-        assert.throws(() => {
+        expect(() => {
             throw new Error('Not implemented');
-        });
+        }).to.throw();
     });
 });
 
 describe('User Controller', () => {
     it('Issue3: Create the registration of a new user to controllers/users.js inside the registerUser() function', () => {
         // Placeholder test, replace with actual test code
-        assert.throws(() => {
+        expect(() => {
             throw new Error('Not implemented');
-        });
+        }).to.throw();
     });
 
     it('Issue4: Add a check that the current user cannot update his/her own data', () => {
         // Placeholder test, replace with actual test code
-        assert.throws(() => {
+        expect(() => {
             throw new Error('Not implemented');
-        });
+        }).to.throw();
     });
 
     it('Issue5: Create a user update to controllers/users.js inside the updateUser() function', () => {
         // Placeholder test, replace with actual test code
-        assert.throws(() => {
+        expect(() => {
             throw new Error('Not implemented');
-        });
+        }).to.throw();
     });
 
     it('Issue6: Add a check that the current user cannot delete his/her own data', () => {
         // Placeholder test, replace with actual test code
-        assert.throws(() => {
+        expect(() => {
             throw new Error('Not implemented');
-        });
+        }).to.throw();
     });
 
     it('Issue7: Create the deletion of a single user to controllers/users.js inside the deleteUser() function', () => {
         // Placeholder test, replace with actual test code
-        assert.throws(() => {
+        expect(() => {
             throw new Error('Not implemented');
-        });
+        }).to.throw();
     });
 
     it('Issue8: Create a user view to controllers/users.js inside the viewUser() function', () => {
         // Placeholder test, replace with actual test code
-        assert.throws(() => {
+        expect(() => {
             throw new Error('Not implemented');
-        });
+        }).to.throw();
     });
 
     it('Issue9: Create a user list to controllers/users.js inside the getAllUsers() function', () => {
         // Placeholder test, replace with actual test code
-        assert.throws(() => {
+        expect(() => {
             throw new Error('Not implemented');
-        });
+        }).to.throw();
     });
 });
 
 describe('Product Controller', () => {
     it('Issue10: Create a product list to controllers/products.js inside the getAllProducts() function', () => {
         // Placeholder test, replace with actual test code
-        assert.throws(() => {
+        expect(() => {
             throw new Error('Not implemented');
-        });
+        }).to.throw();
     });
 });
