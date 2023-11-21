4c2930156239804e1a252f2cdf2360e9c401149e
Minh Nguyen <minh.q.nguyen@tuni.fi>
2023-11-22 00:11:23 +0200

    feat: GET /api/orders and GET /api/orders/{orderId}

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,1 +1,29 @@
 const Order = require("../models/order");
+const { sendJson, badRequest, notFound } = require("../utils/responseUtils");
+const { getCurrentUser } = require("../auth/auth");
+
+const getOrders = async (response, currentUSer) => {
+  //const currentUser = await getCurrentUser(request);
+  if (currentUSer.role === "admin") {
+    const orders = await Order.find({}).exec();
+    return sendJson(response, orders);
+  } else {
+    const orders = await Order.find({ customerId: currentUSer._id }).exec();
+    return sendJson(response, orders);
+  }
+};
+
+const viewOrder = async (response, orderId, currentUser) => {
+  if (currentUser.role === "admin") {
+    const order = await Order.findById({ _id: orderId }).exec();
+    return order ? sendJson(response, order) : notFound(response, "404 Not Found");
+  } else {
+    const order = await Order.findOne({ _id: orderId, customerId: currentUser._id }).exec();
+    return order ? sendJson(response, order) : notFound(response, "404 Not Found");
+  }
+};
+
+module.exports = {
+  getOrders,
+  viewOrder
+};
--- routers/orders.js
+++ routers/orders.js
@@ -1,3 +1,32 @@
-const orderRouter = {};
+const { getCurrentUser } = require("../auth/auth");
+const {
+  getOrders,
+  viewOrder
+} = require("../controllers/orders");
+const { isJson, parseBodyJson, acceptsJson } = require("../utils/requestUtils");
+const {
+  basicAuthChallenge,
+  forbidden,
+  badRequest,
+  contentTypeNotAcceptable,
+} = require("../utils/responseUtils");
+
+const get = async (path, request, response) => {
+  if (!acceptsJson(request)) {
+    return contentTypeNotAcceptable(response);
+  }
+  const currentUser = await getCurrentUser(request);
+  if (!currentUser) return basicAuthChallenge(response);
+
+  const pathArr = path.split("/");
+  if (pathArr.length === 3) {
+    return await getOrders(response, currentUser);
+  } else if (pathArr.length === 4) {
+    return await viewOrder(response, pathArr[3], currentUser);
+  }
+  return badRequest(response, "400 Bad Request");
+};
+
+const orderRouter = { get };
 
 module.exports = orderRouter;
